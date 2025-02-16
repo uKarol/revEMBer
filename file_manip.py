e24c9178d73795f30e0f5227cd15a2577d49744d
uKarol <karol.ujda@gmail.com>
2025-02-16 23:09:27 +0100

    fix problem with file manip

--- file_manip.py
+++ file_manip.py
@@ -5,6 +5,11 @@ class CFileManip:
     def __init__(self):
         self.include = '#include "revEMBer.h"\n'
         self.comment = "/* \nTHIS FILE HAS ADDED DEBUG INFORMATIONS \n revEMBer projct in github: https://github.com/uKarol/revEMBer \njefvcoe oefpm d actmdhsae\n*/\n"
+        
+        self.to_be_added = { "begin" : ' /* function begin */ ',
+                             "ret" : ' /* function return */ ',
+                             "end" : '/* function end */ ',
+        }   
 
     def add_include_info(self, lines):
         lines[0] = self.comment + self.include + lines[0]
@@ -53,18 +58,18 @@ class CFileManip:
             function_rets = functions_to_change.returns
             function_end_ln = functions_to_change.end
             
-            lines[function_begin_ln] = self.add_sequence_in_begin(lines[function_begin_ln], ' /* function begin */ \n')
+            lines[function_begin_ln] = self.add_sequence_in_begin(lines[function_begin_ln], self.to_be_added["begin"] + ' \n')
 
             if type(function_rets) == int: 
-                lines[function_rets] = self.add_sequence_in_return(lines[function_rets], ' /* function return */ \n')
+                lines[function_rets] = self.add_sequence_in_return(lines[function_rets], self.to_be_added["ret"] + ' \n')
             elif len(function_rets) > 0:
                 for ret in function_rets.split():
                     ret = int(ret)
-                    lines[ret] = self.add_sequence_in_return(lines[ret], ' /* function return */ \n')
+                    lines[ret] = self.add_sequence_in_return(lines[ret], self.to_be_added["ret"] + ' \n')
             
             else: 
                 print(function_rets.split())
-                lines[function_end_ln] = self.add_sequence_in_end(lines[function_end_ln], ' /* function end */ \n')
+                lines[function_end_ln] = self.add_sequence_in_end(lines[function_end_ln], self.to_be_added["end"] + ' \n')
 
 
     def add_dbg_functions(self, filepath, functions_to_change: list):
