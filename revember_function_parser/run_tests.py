ca4289d8ee6d969736b56e4782dcac4ac4b88b94
uKarol <karol.ujda@gmail.com>
2025-03-22 13:25:13 +0100

    exception handling in test runner

--- revember_function_parser/run_tests.py
+++ revember_function_parser/run_tests.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         detector.search_file(os.getcwd()+basic_files+file)
         f_dict = detector.get_found_functions()
         result_file_name = os.getcwd()+results_dir+file+".json"
-        out_file = open(result_file_name, "r+")
+        out_file = open(result_file_name, "w+")
         out_file.truncate(0)
         out_file.close()
         out_file = open(result_file_name, "a")
@@ -29,5 +29,8 @@ if __name__ == "__main__":
             json.dump( dataclasses.asdict(f_dict[function]), out_file, indent=6)
         out_file.close()
         expected_file_name = os.getcwd()+expected_dir+file+".json"
-        print(f'file {file} verdict: {filecmp.cmp(result_file_name, expected_file_name)}')
+        try:
+            print(f'file {file} verdict: {filecmp.cmp(result_file_name, expected_file_name)}')
+        except FileNotFoundError: 
+            print(f'file {file} not found')
         detector.clear_results()
